1671d5a85d6a79bf66a954b1bffddd41766ed76c
yan buxes <yanbuxes@gmail.com>
2025-09-02 16:34:28 -0300

    new & created

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,21 @@
 class ChatsController < ApplicationController
+  def new
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = current_user.chats.new(chat_params)
+
+    if @chat.save
+      redirect_to @chat, notice: "Chat created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:game_id)
+  end
 end
