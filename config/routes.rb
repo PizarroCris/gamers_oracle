defa6b9eaa150b6a9c9106b40d42a2ae098739ea
yan buxes <yanbuxes@gmail.com>
2025-09-03 17:19:51 -0300

    update chat open ai

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # Healthcheck
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Home
   root to: "pages#home"
 
-  resources :chats
+  # Chats + AI endpoint
+  resources :chats do
+    member do
+      post :ask_ai   # -> ask_ai_chat_path(@chat)
+    end
+  end
+
+  # Games
   resources :games
 end
