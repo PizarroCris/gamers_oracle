34c262a8debd24f4657c54f31e53b12339270fe1
Caio Figueiredo <47697760+CAiAuM@users.noreply.github.com>
2025-09-03 12:27:01 -0300

    configurando controller de games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,10 +3,23 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
     @chat = @game.chat
   end
- 
+
   def new
+    @games = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to @game
+    else
+      render  :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:name).permit(:name, :image)
   end
 end
