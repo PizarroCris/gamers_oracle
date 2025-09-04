520ab03fa6e6315f124428156f525ec39df7b899
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-04 14:44:56 +0100

    add_messages_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # Healthcheck
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Home
   root to: "pages#home"
 
-  # Chats + AI endpoint
   resources :chats do
-    member do
-      post :ask_ai   # -> ask_ai_chat_path(@chat)
-    end
+    resources :messages, only: [:new, :create]
   end
 
-  # Games
-  resources :games
+  resources :games do
+    resources :chats, only: [:index, :create]
+  end
 end
