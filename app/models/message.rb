e9d4197d8964209605968f9303fa7144c1595aaa
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-09-05 15:29:54 +0100

    making functional broadcast

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,14 +1,23 @@
 class Message < ApplicationRecord
   acts_as_message
 
-  validates :content, length: { minimum: 10, maximum: 1000 }, if: -> { role == "user" }
+  validates :content, length: { minimum: 3, maximum: 1000 }, if: -> { role == "user" }
   validates :role, presence: true
   validates :chat, presence: true
 
   MAX_USER_MESSAGES = 10
   validate :user_message_limit, if: -> { role == "user" }
 
+  after_create_commit :broadcast_append_to_chat
+  
+
   private
+
+  def broadcast_append_to_chat
+    return unless role.in?(["user", "assistant"])
+
+    broadcast_append_to chat, target: "messages", partial: "messages/message", locals: { message: self }
+  end
 
   def user_message_limit
     if chat.messages.where(role: "user").count >= MAX_USER_MESSAGES
